c5b325019eccc9dd868bf3257c0f1cf79bc36f27
yvzmtl <selim.mutlu@bil.omu.edu.tr>
2014-12-07 17:20:02 +0200

    Update randomName.c

--- randomName.c
+++ randomName.c
@@ -3,102 +3,102 @@
 #include<stdlib.h>
 #include<time.h>
 
-#define size 10
-struct Random{
-	char language;
-	int  adet;
-	char secenek;
-}RandomName;
-int main(void){
-	
-	bool checkWord();
-	char word_list[size][100];
-	
-	char isim[size][100] ={"kedi","insan","deniz","masa","dolap","bilgisayar",
+#define size 50
+
+	char isim[][20] ={"kedi","insan","deniz","masa","dolap","bilgisayar",
 			"kapi","pencere","hafta","gece"	};
 	
-	char sifat[size][100]= {"iyi","guzel","hizli","yavas","yakisikli","mert",
+	char sifat[][20]= {"iyi","guzel","hizli","yavas","yakisikli","mert",
 			"cimri","kel","masmavi"};
 		
-	char name[size][100] = {"baby","child","man","main","house","sea","car",
+	char name[][20] = {"baby","child","man","main","house","sea","car",
 			"computer","pencil","pen"};
 			
-	char adjectives[size][100] = {"good","beatiful","long","small","nice",
+	char adjectives[][20] = {"good","beatiful","long","small","nice",
 			"micke","bad","slow","green","blue"};
-	
-	//char str[10];
-	//str = "eng";		
-	scanf("%s", RandomName.language);
-	 
 
-	if(RandomName.language == 'eng' ){
-		 //int name_boyut = strlen(name) ;
-		 //int adjective_boyut = strlen(adjectives);
+ int CheckWord(char* kelime);
+ void NewTrWord(char* tr );
+ void NewEngWord(char* eng );
+ 
+ char WordList[size]={};
+ int main(int argc, char* argv[]){
+	char *language = argv[1];
+	int repeat = argc;
+	
+	char kelime[size];
+	
+	if(strcmp(language, "eng") ==0){
+		 
 		int i=0;		
-		while( i < RandomName.adet ){
+		while( i < repeat )
+		{
 			srand(time(NULL));
-			int index_name = rand()%10;
-			int index_adjectives = rand()%10;
 			
-			char kelime [50];
-			strcpy (kelime, adjectives[index_adjectives]);
-			strcat(kelime,  name[index_name]);
 			
-			//char s1[100] = adjectives[index_adjectives];
-			//char s2[100] =  name[index_name];
-			//char kelime = strcat( s1,s2);
 			printf("\n");
-			
-			
-			bool kelime2 = checkWord(kelime) ;
-			
-			if(kelime2){
-				word_list.append(kelime);
-				i++;
-				printf(kelime);
-			}
-			
+
+			int state = CheckWord(kelime);
+				if(state == 0)
+					continue;
+				else{
+					NewTrWord(kelime);
+					strcat(WordList,kelime);
+					puts(kelime);
+				}
+			i++;
 		}
 	}
 		
 	else{
-		//int isim_boyut = strlen(isim);
-		//int sifat_boyut = strlen(sifat);
-		
+	
 		int j=0;
-		while(j < RandomName.adet){
-			
-			int index_isim = rand()%10;
-			int index_sifat = rand()%10;
-			char kelime3 [50];
-			strcpy (kelime3, sifat[index_sifat]);
-			strcat(kelime3,  isim[index_isim]);
-			
+		while(j < repeat)
+		{
+			srand(time(NULL));
 			printf("\n");
 			
-			if(checkWord(kelime)){
-				word_list.append(kelime2);
-				j++;
-				printf(kelime);
-			}
+			int state = CheckWord(kelime);
+				if(state == 0)
+					continue;
+				else{
+					NewEngWord(kelime);
+					strcat(WordList,kelime);	
+					puts(kelime);
+				}
+			j++;	
 		}
 	}
-	return 0;
+	
 }
+	int CheckWord(char* kelime)
+	{
+		int k=0;
+		for( k ; k < size ; k++)
+		{
+			if(strcmp(WordList,kelime)==0)
+				return 0;
+			else
+				return 1;
+	    }
+	}	
+
 		
-		bool checkWord( char kelime){
+	void NewTrWord(char* tr ){
+		tr[0]=0;
+		strcat(tr,sifat[(rand()%sizeof(sifat))]);
+		strcat(tr,isim[rand()%sizeof(isim)]);
+		return;
+	}
+	
+	void NewEngWord(char* eng ){
+		eng[0]=0;
+		strcat(eng,adjectives[(rand()%sizeof(adjectives))]);
+		strcat(eng,isim[rand()%sizeof(name)]);
+		return;
+	}	
 			
-			int i=0;
-			
-			for(i;i<strlen(word_list);i++){
-				if(strcmp(word_list[i] , kelime)== 0)
-					return false ;
-				
-				else
-					return true;
-			}
-			
-		}
+	
 	
 
 	
