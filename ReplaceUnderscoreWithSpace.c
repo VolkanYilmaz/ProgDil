761b3c0874d227c41fbc3aee247527c9f750b46c
Hakan KABASAKAL <hakan.kabasakal@bil.omu.edu.tr>
2014-12-20 23:58:07 +0200

    düzenleme

--- ReplaceUnderscoreWithSpace.c
+++ ReplaceUnderscoreWithSpace.c
@@ -2,27 +2,22 @@
 #include <string.h>
 
 int main(int argc, char* argv[])
-{	
-    int i;
-    int first = 0;
-    char *text = argv[1];
-    int last = strlen(text)-1;
-    
-    while( text[first] == '_')
-    {
-    	first++;
-    }
-    
-    while( text[last] == '_')
-    {
-    	last--;
-    }
-     
-    for(i=first; i<last; i++)
-    {  
-        if( text[i] == '_' )
-            text[i] = ' ';    
-    }  
-    printf( " %s \n" , text); 
-	return 0;     
+{
+	int i,first = 0;
+	char *text = argv[1];
+	int last = strlen(text)-1;
+
+	while(text[first] == '_')
+   		first++;
+	while(text[last] == '_')
+   		last--;
+	for(i=first;i<last;i++)
+	{
+		if(text[i] == '_')
+			text[i] = ' ';
+	}
+	
+	printf("%s \n", text);
+
+	return 0;
 }
