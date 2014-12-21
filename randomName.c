b8726f74e2df083987959138044f5002c4b2b78a
yvzmtl <selim.mutlu@bil.omu.edu.tr>
2014-12-21 23:14:57 +0200

    Update randomName.c
    Güncelleme

--- randomName.c
+++ randomName.c
@@ -1,104 +1,105 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<time.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define size 50
-
-	char isim[][20] ={"kedi","insan","deniz","masa","dolap","bilgisayar",
-			"kapi","pencere","hafta","gece"	};
+char isim[][30] ={
+	"kedi","insan","deniz","masa","dolap","bilgisayar",
+	 "kapi","pencere","hafta","gece","gunduz","marti","kazak",
+	 "sarisin","sahil","merdiven","yorgan","yol","araba","agac",
+	 "yaprak","dal","adam","konuk","sabah","koca","kadin","sÄ±ra","Ã¶ÄŸrenci","yatak"
+};
 	
-	char sifat[][20]= {"iyi","guzel","hizli","yavas","yakisikli","mert",
-			"cimri","kel","masmavi"};
+char sifat[][30]= {
+	"iyi","guzel","hizli","yavas","yakisikli","mert","bir",
+	"cimri","kel","masmavi","ucan","kaliteli","yarim","bu","ÅŸu",
+	"kÄ±rmÄ±zÄ±","pahalÄ±","ilginÃ§","uzun","kÄ±sa","yamuk","ucuz","tuhaf",
+	"Ã§irkin","ÅŸiÅŸman","zayÄ±f","eski","yeni","sivri","nazik"	
+};
 		
-	char name[][20] = {"baby","child","man","main","house","sea","car",
-			"computer","pencil","pen"};
+char name[][30] = {
+	"baby","child","man","main","house","sea","car",
+	"computer","pencil","pen","doctor","window","line","cat","human",
+	"beach","bed","woman","night","morning","tree","door","student",
+	"teacher","table","week","day","plane","bag","shoes"
+};
 			
-	char adjectives[][20] = {"good","beatiful","long","small","nice",
-			"micke","bad","slow","green","blue"};
+char adjective[][30] = {
+	"good","beatiful","long","small","nice","kind","new",
+	"micke","bad","slow","green","blue","hard","old","slow",
+	"fast","interesting","clever","bent","real","sharp","thin","fat",
+	"easy","hard","expensive","cheap","ugly","half","red"
+};
+	
+char trgenerator(char *kelime );
+char enggenerator(char *kelime );
+int check(char *kelime, char list[][30]);
 
- int CheckWord(char* kelime);
- void NewTrWord(char* tr );
- void NewEngWord(char* eng );
- 
- char WordList[size]={};
- int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 	char *language = argv[1];
-	int repeat = argc;
+	int again = atoi(argv[2]);
 	
-	char kelime[size];
-	
-	if(strcmp(language, "eng") ==0){
-		 
-		int i=0;		
-		while( i < repeat )
-		{
-			srand(time(NULL));
-			
-			
-			printf("\n");
-
-			int state = CheckWord(kelime);
-				if(state == 0)
-					continue;
-				else{
-					NewTrWord(kelime);
-					strcat(WordList,kelime);
-					puts(kelime);
-				}
+	char wordlist[30][30];
+	char kelime[30];
+	srand(time(NULL));
+	if(strcmp(language, "eng") == 0){	
+		int i=0;
+		while(i<again){
+			enggenerator(kelime);	
+			int state = check(kelime,wordlist);
+			if(state==1){
+				puts(kelime);
+				strcat(*wordlist,kelime);
+			}
+			else
+				continue;
 			i++;
 		}
 	}
-		
 	else{
-	
 		int j=0;
-		while(j < repeat)
-		{
-			srand(time(NULL));
-			printf("\n");
-			
-			int state = CheckWord(kelime);
-				if(state == 0)
-					continue;
-				else{
-					NewEngWord(kelime);
-					strcat(WordList,kelime);	
-					puts(kelime);
-				}
-			j++;	
-		}
-	}
-	
-}
-	int CheckWord(char* kelime)
-	{
-		int k=0;
-		for( k ; k < size ; k++)
-		{
-			if(strcmp(WordList,kelime)==0)
-				return 0;
+		while(j<again){
+			trgenerator(kelime);
+			//strcpy(kelime, trgenerator(kelime));	
+			int state=check(kelime,wordlist);
+			if(state==1){
+				puts(kelime);
+				strcat(*wordlist,kelime);
+			}
 			else
-				return 1;
-	    }
+				continue;
+			j++;
+		}	
 	}	
+}
 
-		
-	void NewTrWord(char* tr ){
-		tr[0]=0;
-		strcat(tr,sifat[(rand()%sizeof(sifat))]);
-		strcat(tr,isim[rand()%sizeof(isim)]);
-		return;
-	}
-	
-	void NewEngWord(char* eng ){
-		eng[0]=0;
-		strcat(eng,adjectives[(rand()%sizeof(adjectives))]);
-		strcat(eng,isim[rand()%sizeof(name)]);
-		return;
+char trgenerator(char *kelime)
+{	
+	kelime[0]=0;
+	strcat(kelime, sifat[rand()%30]);
+	strcat(kelime, " ");
+	strcat(kelime, isim[rand()%30]);
+	return *kelime;
+}
+
+char enggenerator(char *kelime)
+{
+	kelime[0]=0;
+	strcat(kelime, adjective[rand()%30]);
+	strcat(kelime, " ");
+	strcat(kelime, name[rand()%30]);
+	return *kelime;
+}
+
+int check(char *kelime, char list[][30])
+{
+	int i=0;	
+	for(i;i<30;i++){
+		int state = strcmp(list[i],kelime);
+		if(state == 0){
+			return 0;}
+		else{
+			return 1;}
 	}	
-			
-	
-	
-
-	
+}
