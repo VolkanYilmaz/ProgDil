87f65ab46663d69b2bbee97ff4a7f260fd88460e
yvzmtl <selim.mutlu@bil.omu.edu.tr>
2014-12-24 13:18:08 +0200

    Düzenleme

--- RandomName.c
+++ RandomName.c
@@ -1,24 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 #include "RandomName.h"
 
-//TanÄ±mlÄ± sabitler. Karakter dizileri iÃ§in gerekli.
+//Tanımlı sabitler. Karakter dizileri için gerekli.
 #define ROW 2
 #define COLUMN 15
 
 int main(int argc, char *argv[]){ 
-	
-	int i;                   //sayaÃ§ gÃ¶revindeki deÄŸiÅŸken
+	setlocale(LC_ALL,"turkish");
+	int i;                   //sayaç göevindeki değişken
 	srand(time(NULL));		 //rasgele fonksiyonunu tamamlayan kod.
 	
 	char *adj[ROW][COLUMN]={
-		{"iyi","kÃ¶tÃ¼","gÃ¼zel","Ã§irkin","kÄ±rmÄ±z","mavi","siyah","tuzlu","saÄŸlam","Ã§Ã¼rÃ¼k","hÄ±zlÄ±","etkili","yavaÅŸ","akÄ±llÄ±","kurnaz"},
-		{"good","bad","beautiful","ugly","red","blue","black","salty","brave","quick","effective","slow","creative","intelligent","shiny"}
+		{"iyi",
+		"kötü",
+		"güzel",
+		"çirkin",
+		"kırmızı",
+		"mavi",
+		"siyah",
+		"tuzlu",
+		"sağlam",
+		"çürük",
+		"hızlı",
+		"etkili",
+		"yavaş",
+		"akıllı",
+		"kurnaz"},
+		{"good",
+		"bad",
+		"beautiful",
+		"ugly",
+		"red",
+		"blue",
+		"black",
+		"salty",
+		"brave",
+		"quick",
+		"effective",
+		"slow",
+		"creative",
+		"intelligent",
+		"shiny"}
 	};
 
 	char *noun[ROW][COLUMN]={
-		{"insan","aslan","saat","ÅŸeker","pamuk","masa","araba","cetvel","kalem","monitÃ¶r","piyano","gitar","dÃ¼nya","gÃ¼neÅŸ","eldiven"},
-		{"person","lion","leopard","table","world","clock","sun","arrow","book","machine","eagle","rabbit","terminator","university","bus"}
+		{"insan",
+		"aslan",
+		"saat",
+		"şeker",
+		"pamuk",
+		"masa",
+		"araba",
+		"cetvel",
+		"kalem",
+		"monitör",
+		"piyano",
+		"gitar",
+		"dünya",
+		"güneş",
+		"eldiven"},
+		{"person",
+		"lion",
+		"leopard",
+		"table",
+		"world",
+		"clock",
+		"sun",
+		"arrow",
+		"book",
+		"machine",
+		"eagle",
+		"rabbit",
+		"terminator",
+		"university",
+		"bus"}
 	};
 	
 	Parameter parameters;
